cbfedcf679d5cfec7aa6bb129f1abe195209f0cc
John Wason <wason@wasontech.com>
2022-02-24 20:03:41 -0500

    Initial commit

---
+++ recipe/build_common.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+  -DCMAKE_PREFIX_PATH:PATH=$PREFIX \
+  -DCMAKE_BUILD_TYPE:STRING=Release \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DBUILD_SHARED_LIBS=ON \
+  -S tesseract_common \
+  -B build_dir
+
+cmake --build build_dir --config Release -- -j$CPU_COUNT
+cmake --build build_dir --config Release --target install
