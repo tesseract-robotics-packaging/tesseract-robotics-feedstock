2497becfb40b698eab503890a26880cb65f3f534
John Wason <wason@wasontech.com>
2026-05-29 17:37:02 -0400

    Improve build and tests

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,7 +2,7 @@
 
 if exist src rmdir /s /q src
 mkdir src
-echo */ci/* > tar_excludes.txt
+echo */ci/*> tar_excludes.txt
 tar xf source.tar.gz --strip-components=1 -C src -X tar_excludes.txt
 
 cmake -GNinja ^
@@ -12,6 +12,8 @@ cmake -GNinja ^
   -DBUILD_SHARED_LIBS=ON ^
   -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON ^
   -DTESSERACT_ENABLE_TESTING=OFF ^
+  -DTESSERACT_ENABLE_EXAMPLES=OFF ^
+  -DCMAKE_VERBOSE_MAKEFILE=ON ^
   -S src ^
   -B build_dir
 if %errorlevel% neq 0 exit /b %errorlevel%
